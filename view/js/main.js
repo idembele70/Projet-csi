9f21e0792625fcd0bc5c1444d1f4cc524be4173d
idembele70 <idembele70@gmail.com>
2021-02-26 20:10:51 +0100

    hamburger menu animation p1

--- view/js/main.js
+++ view/js/main.js
@@ -1,18 +1,22 @@
 const hamburgerList = document.querySelector('.hamburger-list');
-const hamburgerItem = document.querySelector('.hamburger-item');
+const hamburgerItem = document.querySelectorAll('.hamburger-item-disable');
 
 hamburgerList.addEventListener('click',()=>{
 if (hamburgerList.classList.contains('hamburger-list-disable')) {
-    hamburgerList.classList.remove('hamburger-list-disable')
-    hamburgerList.classList.add('hamburger-list-enable')
-    
-    hamburgerItem.classList.remove('hamburger-item-disable')
-    hamburgerItem.classList.add('hamburger-item-enable')
+    hamburgerList.classList.remove('hamburger-list-disable');
+    hamburgerList.classList.add('hamburger-list-enable');
+    hamburgerItem.forEach((element)=>{
+        element.classList.remove('hamburger-item-disable');
+        element.classList.add('hamburger-item-enable');
+    })
 } else {
-    hamburgerList.classList.remove('hamburger-list-enable')
-    hamburgerList.classList.add('hamburger-list-disable')
-
-    hamburgerItem.classList.remove('hamburger-item-enable')
-    hamburgerItem.classList.add('hamburger-item-disable')
+    hamburgerList.classList.remove('hamburger-list-enable');
+    hamburgerList.classList.add('hamburger-list-disable');
+    
+    hamburgerItem.forEach((element)=>{
+    element.classList.remove('hamburger-item-enable');
+    element.classList.add('hamburger-item-disable');
+    })
+   
 }
 },false)
