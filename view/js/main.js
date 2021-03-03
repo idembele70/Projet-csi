7df92eff8cd0ff59c81c22e3cca79e2a90d6fdb3
idembele70 <idembele70@gmail.com>
2021-03-03 19:07:45 +0100

    header pour visiteur non connecté / inscrit

--- view/js/main.js
+++ view/js/main.js
@@ -1,6 +1,7 @@
 const hamburgerList = document.querySelector('.hamburger-list');
 const hamburgerItem = document.querySelectorAll('.hamburger-item-disable');
 const header = document.querySelector('.header');
+const navList = document.querySelector('.nav-list');
 
 hamburgerList.addEventListener('click',()=>{
     // navbar
@@ -16,21 +17,28 @@ if (hamburgerList.classList.contains('hamburger-list-disable')) {
     hamburgerItem.forEach((element)=>{
         element.classList.remove('hamburger-item-disable');
         element.classList.add('hamburger-item-enable');
-
+        
     });
+    // afficher 
+    // navList.classList.remove('nav-list');
+    navList.classList.remove('nav-list');
+    navList.classList.add('nav-list-enable');
     //Desactiver navbar
 } else {
-    // 
+    // mettre le header a disable
     header.classList.remove('header-enable');
     header.classList.add('header-disable');
-    // 
+    
+    // desactive le hamburgeur et ses items
     hamburgerList.classList.remove('hamburger-list-enable');
     hamburgerList.classList.add('hamburger-list-disable');
-    
     hamburgerItem.forEach((element)=>{
-    element.classList.remove('hamburger-item-enable');
-    element.classList.add('hamburger-item-disable');
+        element.classList.remove('hamburger-item-enable');
+        element.classList.add('hamburger-item-disable');
     })
+    // cache navlist
+    navList.classList.remove('nav-list-enable');
+    navList.classList.add('nav-list');
    
 }
 },false)
