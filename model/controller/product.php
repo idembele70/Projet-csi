6333eb1f2a6ae57e22748c88acc43272eb35a232
jonathanBouygues <jonathanbouygues@yahoo.fr>
2021-03-04 14:04:00 +0100

    first push espace perso

--- model/controller/product.php
+++ model/controller/product.php
@@ -1,5 +1,13 @@
 <?php
 
+// Include header
+include('header.php');
+
+
 // Code PHP
 include('function/renderProduct.php');
 renderProduct($idProduct ?? 2);
+
+
+// Include footer
+include('footer.php');
