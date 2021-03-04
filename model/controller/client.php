321cdf8cce19e549260c784468bf641ac228bb62
Avaadora <manonber31@gmail.com>
2021-03-04 15:29:06 +0100

    Reset du mot de passe client avec le mail associé

--- model/controller/client.php
+++ model/controller/client.php
@@ -67,8 +67,45 @@
 	    }
 	}
 
-	function FunctionName($value='')
+	// Vide le champ mot de passe pour être remplacé
+	function updateMotDePasse($adresseMail, $motDePasse, $token){
+		try{
+			$token = bin2hex(random_bytes(12))
+			$con = getDatabaseConnexion();
+			$motDePasse =  hash("sha256", $this->motDePasse)
+			$message= "<table style='border-radius:15px; border: 3px solid #6247c2; text-align:center; padding:20px; font-family:roboto; background-color:#ecf3e5'>";
+	        $message.= "<tr>";
+	        $message.= "<td><img src='logo.png' alt='logo-playduh' style='width:50px'></td>";
+	        $message.= "</tr>";
+	        $message.= "<tr>";
+	        $message.= "<td><h1 style='font-size:25px; color:#90c9e0;margin:10px'>Votre demande a bien été reçue</h1></td>";
+	        $message.= "</tr>";
+	        $message.= "<tr>";
+	        $message.= "<td><p style='font-size:15px; color:#90c9e0; margin:10px'>Cliquez sur ce <a href="'#'">lien</a> pour réinitialiser votre mot de passe</p></td>";
+	        $message.= "</tr>";
+	        $message.= "<tr>";
+	        $message.= "<td><p style='margin:10px; font-size:10px; color:#90c9e0'>&copy; Playduh</p></td>";
+	        $message.= "</tr></table>";
+
+			$requete = "UPDATE client set
+						motDePasse = '',
+						token = '$token'"
+			mail($adresseMail, 'Réinitialisation du mot de passe', $message);
+
+		}catch(PDOException $e){
+			echo $sql . "<br>" . $e->getMessage();
+		}
+	}
+
+	//Mise en place du nouveau mot de passe
+	function replaceMotDePasse($motDePasse)
 	{
-		# code...
+		try{
+			$requete = "UPDATE client set
+			motDePasse = '$motDePasse',
+			token = ''"
+		}catch(PDOException $e){
+			echo $sql . "<br>" . $e->getMessage();
+		}
 	}
 ?>
