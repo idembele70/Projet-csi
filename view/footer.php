0981b233f9ac97595c23871300793e59656416fa
jonathanBouygues <jonathanbouygues@yahoo.fr>
2021-05-12 11:15:27 +0200

    cgv & mentions data

--- view/footer.php
+++ view/footer.php
@@ -1,10 +1,10 @@
 <footer>
     <div class="contenu-footer">
         <div class= "bloc footer-un">
-            <h3>Pikachu</h3>
+        <h3><a href="./cgv">CGV</a></h3>
         </div>
         <div class= "bloc footer-deux">
-            <h3>Mario</h3>
+            <h3><a href="./mentions_legales">Mentions légales</a></h3>
         </div>
         <div class= "bloc footer-contact">
             <h3><a href="./contact">Contactez-nous</a></h3>
